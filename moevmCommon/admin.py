35f77c26def4c671a071da1c49a424c93643e52e
w3prog <w3prog@gmail.com>
2016-10-30 15:34:05 +0300

    Исправление с подключением моделей в административную панель

--- moevmCommon/admin.py
+++ moevmCommon/admin.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 from django.contrib import admin
-from .models import *
+from moevmCommon.models.userProfile import *
+from moevmCommon.models.academicDiscipline import *
+from moevmCommon.models.nir import *
+from moevmCommon.models.publication import *
+from moevmCommon.models.scientificEvent import *
 
 
-admin.site.register(Person)
+admin.site.register(UserProfile)
 admin.site.register(AcademicDiscipline)
 admin.site.register(AcademicDisciplineOfTeacher)
 admin.site.register(NIR)
 admin.site.register(ScientificEvent)
+admin.site.register(Participation)
 admin.site.register(Publication)
