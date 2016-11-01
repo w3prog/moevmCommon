58f68ae7082796e925cce249e8543e4c45f09875
w3prog <w3prog@gmail.com>
2016-11-01 20:13:56 +0300

    Добавил лицензию

--- moevmCommon/management/commands/seedfakedata.py
+++ moevmCommon/management/commands/seedfakedata.py
@@ -1,3 +1,28 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-# Create your models here.
+from django.core.management import BaseCommand
+from moevmCommon.models.userProfile import *
+from moevmCommon.models.academicDiscipline import *
+from moevmCommon.models.nir import *
+from moevmCommon.models.publication import *
+from moevmCommon.models.scientificEvent import *
+from django.contrib.auth.models import User
+from faker import Faker
+
+
+class Command(BaseCommand):
+  """
+  Данная команда необходима чтобы наполнить базу новыми данными.
+  """
+
+  def handle(self, *args, **options):
+    """
+    Данная процедура создает новые данные в базу
+    :param args:
+    :param options:
+    :return:
+    """
+    faker = Faker()
+    #todo реализовать процедуру.
+    print faker.name()
+    print("Случайные данные добавлены в базу данных")
