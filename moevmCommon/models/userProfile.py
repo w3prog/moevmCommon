c3e2c4a2d769f30c10ab987a6209962609f3a9f4
Evgeniy Gorodilov <gorodilov.box@gmail.com>
2016-11-20 18:13:41 +0300

    [Add] Unicode method + collection field

--- moevmCommon/models/userProfile.py
+++ moevmCommon/models/userProfile.py
@@ -75,8 +75,11 @@ class UserProfile(models.Model):
     max_length=1,
     choices=ACADEMIC_STATUS_CHOICES,
     null=True)
+  
   year_of_academic_status = models.DateField(null=True)
 
+  profiles = UserProfileManager()
+    
   @property
   def first_name(self):
     return self.user.first_name
@@ -97,57 +100,11 @@ class UserProfile(models.Model):
   def email(self):
     return self.user.email
 
-  @staticmethod
-  def create(login, password, email, **params):
-    user = User.objects.create_user(login, password, email)
-    user.first_name = params.get('first_name')
-    user.last_name = params.get('last_name')
-    user.save()
-
-    user_profile = UserProfile.objects.create(
-      user=user,
-      patronymic=params.get('patronymic'),
-      birth_date=params.get('birth_date'),
-      study_group=params.get('study_group'),
-      github_id=params.get('github_id'),
-      stepic_id=params.get('stepic_id'),
-      type=params.get('type', 's'),
-      election_date=params.get('election_date'),
-      position=params.get('position'),
-      contract_date=params.get('contract_date'),
-      academic_degree=params.get('academic_degree'),
-      year_of_academic_degree=params.get('year_of_academic_degree'),
-      academic_status=params.get('academic_status'),
-      year_of_academic_status=params.get('year_of_academic_status')
-    )
-
-    user_profile.save()
-
-    return user_profile
-
-  @staticmethod
-  def create_of_user(**params):
-    user_profile = UserProfile.objects.create(
-      user=params.get('user'),
-      patronymic=params.get('patronymic'),
-      birth_date=params.get('birth_date'),
-      study_group=params.get('study_group'),
-      github_id=params.get('github_id'),
-      stepic_id=params.get('stepic_id'),
-      type=params.get('type', 's'),
-      election_date=params.get('election_date'),
-      position=params.get('position'),
-      contract_date=params.get('contract_date'),
-      academic_degree=params.get('academic_degree'),
-      year_of_academic_degree=params.get('year_of_academic_degree'),
-      academic_status=params.get('academic_status'),
-      year_of_academic_status=params.get('year_of_academic_status')
-    )
-
-    return user_profile
-
   def __str__(self):
     return self.first_name + ' ' + self.last_name + ' ' + self.patronymic
+
+  def __unicode__(self):
+    return unicode(self.user) or u''
 
   class Meta:
     db_table = 'userprofiles'
