ca97e4e8351a8bec0cb56d90823ba009bacaeaec
w3prog <w3prog@gmail.com>
2016-12-03 00:46:08 +0300

    Изменил запись

--- moevmCommon/admin.py
+++ moevmCommon/admin.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 from django.contrib import admin
-from moevmCommon.models.userProfile import *
-from moevmCommon.models.academicDiscipline import *
-from moevmCommon.models.nir import *
-from moevmCommon.models.publication import *
-from moevmCommon.models.scientificEvent import *
+from moevmCommon.models.userProfile import UserProfile
+from moevmCommon.models.academicDiscipline import AcademicDiscipline,AcademicDisciplineOfTeacher
+from moevmCommon.models.nir import NIR
+from moevmCommon.models.publication import Publication
+from moevmCommon.models.scientificEvent import ScientificEvent,Participation
 
 
 admin.site.register(UserProfile)
