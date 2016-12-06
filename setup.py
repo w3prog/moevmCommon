0a04348429fc9e41c159c9a68b8dafa97b69ef12
w3prog <w3prog@gmail.com>
2016-12-06 21:03:12 +0300

    До

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 setup(
     name='django-moevmCommon',
     version='0.1',
-    packages=['moevmCommon'],
+    packages=['moevmCommon','moevmCommon.models','moevmCommon.management'],
     include_package_data=True,
     license='MIT License',
     description='Common information modules for MOEVM',
