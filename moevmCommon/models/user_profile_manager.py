ef3263271605ab1de26b729adb084470e92e6262
w3prog <w3prog@gmail.com>
2016-12-07 21:30:37 +0300

    Немного переделал функцию для вызова факера

--- moevmCommon/models/user_profile_manager.py
+++ moevmCommon/models/user_profile_manager.py
@@ -7,7 +7,9 @@ class UserProfileManager(models.Manager):
     def filter(self, **filter_fields):
         return filter_by_foreign_fields(super(UserProfileManager, self), **filter_fields)
     
-    def create(self, username, password, email, **kwargs):
+    def create(self, username,
+               password, email,
+               **kwargs):
 
         user = User(username=username, email=email, password=password)
 
@@ -33,23 +35,24 @@ class UserProfileManager(models.Manager):
             year_of_academic_status=kwargs.get('year_of_academic_status')
         )
 
-    def create_of_user(**params):
-        from .userProfile import UserProfile
-        user_profile = UserProfile.objects.create(
-            user=params.get('user'),
-            patronymic=params.get('patronymic'),
-            birth_date=params.get('birth_date'),
-            study_group=params.get('study_group'),
-            github_id=params.get('github_id'),
-            stepic_id=params.get('stepic_id'),
-            type=params.get('type', 's'),
-            election_date=params.get('election_date'),
-            position=params.get('position'),
-            contract_date=params.get('contract_date'),
-            academic_degree=params.get('academic_degree'),
-            year_of_academic_degree=params.get('year_of_academic_degree'),
-            academic_status=params.get('academic_status'),
-            year_of_academic_status=params.get('year_of_academic_status')
+    def create_from_user(self,
+                         user=None,
+                         position=None,
+                         contract_date=None,
+                         academic_degree=None,
+                         year_of_academic_degree=None,
+                         academic_status=None,
+                         patronymic=None,
+                         **params):
+        user_profile = self.create(
+            user=user,
+            patronymic=patronymic,
+            type=type,
+            position=position,
+            contract_date=contract_date,
+            academic_degree=academic_degree,
+            year_of_academic_degree=year_of_academic_degree,
+            academic_status=academic_status,
         )
 
         return user_profile
