72fc28ad85bd8a389e98ac964af3173b7a6c5394
w3prog <w3prog@gmail.com>
2016-12-07 21:59:24 +0300

    Добавил методы для создание пользователей 3 типов

--- moevmCommon/models/user_profile_manager.py
+++ moevmCommon/models/user_profile_manager.py
@@ -35,6 +35,106 @@ class UserProfileManager(models.Manager):
             year_of_academic_status=kwargs.get('year_of_academic_status')
         )
 
+    def create_student(self,
+                       username=None,
+                       email=None,
+                       password=None,
+                       first_name=None,
+                       last_name=None,
+                       birth_date=None,
+                       patronymic=None,
+                       study_group=None,
+                       github_id=None,
+                       stepic_id=None,
+                       **kwargs):
+      user = User(username=username, email=email, password=password)
+
+      user.first_name = first_name
+      user.last_name = last_name
+      user.is_superuser = False
+      user.save()
+      type = "s"
+      return self.create(
+        user=user,
+        patronymic=patronymic,
+        birth_date=birth_date,
+        study_group=study_group,
+        github_id=github_id,
+        stepic_id=stepic_id,
+        type=type
+      )
+
+    def create_elder(self,
+                       username=None,
+                       email=None,
+                       password=None,
+                       first_name=None,
+                       last_name=None,
+                       birth_date=None,
+                       patronymic=None,
+                       study_group=None,
+                       github_id=None,
+                       stepic_id=None,
+                       **kwargs):
+      user = User(username=username, email=email, password=password)
+      user.first_name = first_name
+      user.last_name = last_name
+      user.is_superuser = False
+      user.save()
+      type = "h"
+      return self.create(
+        user=user,
+        patronymic=patronymic,
+        birth_date=birth_date,
+        study_group=study_group,
+        github_id=github_id,
+        stepic_id=stepic_id,
+        type=type
+      )
+
+    def create_teacher(self,
+                       username=None,
+                       email=None,
+                       password=None,
+                       first_name=None,
+                       last_name=None,
+                       birth_date=None,
+                       patronymic=None,
+                       election_date=None,
+                       position=None,
+                       contract_date=None,
+                       academic_degree=None,
+                       year_of_academic_degree=None,
+                       academic_status=None,
+                       year_of_academic_status=None,
+                       academic_state=None,
+                       github_id=None,
+                       stepic_id=None,
+                       **kwargs):
+      user = User(username=username, email=email, password=password)
+      user.first_name = first_name
+      user.last_name = last_name
+      user.is_superuser = False
+      user.save()
+      type = "t"
+      return self.create(
+        user=user,
+        patronymic=patronymic,
+        birth_date=birth_date,
+        election_date=election_date,
+        github_id=github_id,
+        stepic_id=stepic_id,
+        position=position,
+        contract_date=contract_date,
+        academic_degree=academic_degree,
+        year_of_academic_degree=year_of_academic_degree,
+        academic_status=academic_status,
+        year_of_academic_status=year_of_academic_status,
+        academic_state=academic_state,
+        type=type
+      )
+
+
     def create_from_user(self,
                          user=None,
                          position=None,
